bf8008b5d24636b1aa7c385e2a7a2b6e9defb9d6
laurenma24-meet <139680874+laurenma24-meet@users.noreply.github.com>
2023-07-17 08:58:19 +0300

    Update lauren.py

--- lauren.py
+++ lauren.py
@@ -1,17 +1,25 @@
-def create_youtube_video:
-	 title=input("please enter your title: ")
-     description=input("please enter your description: ")
-     new_youtube_vid= {"title": title, "description":description,"likes": 0 , "dislikes": 0, "comments":{"usernames":""}}
-      return new_youtube_vid
+def create_youtube_video(title, description):
+    return {
+        "title": title,
+        "description": description,
+        "likes": 0,
+        "dislikes": 0,
+        "comments": {}
+    }
+     
+def like(new_youtube_vid):
+	if "likes" in new_youtube_vid:
+		new_youtube_vid["likes"]+=1
+	return new_youtube_vid
 
 def like(new_youtube_vid):
 	if "likes" in new_youtube_vid:
 		new_youtube_vid["likes"]+=1
 	return new_youtube_vid
 
-def new_comment(new_youtube_vid,username,comment_text)
-	new_youtube_vid["comments"]=comment_text
-	return new_youtube_vid
+def new_comment(new_youtube_vid,username,comment_text):
+    new_youtube_video["comments"][username] = comment_text
+    return video
 
 
 
